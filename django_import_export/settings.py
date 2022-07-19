82a4272dc058b2f14a5f2c9cf56be9d2df40beae
rooflexx <lirrishala@gmail.com>
2022-07-19 21:30:51 +0200

    init

--- django_import_export/settings.py
+++ django_import_export/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gr7+kk$!1xb6zhbg3ie@3tm50o!=qalpk7mi9#o5a0uv53$dt1'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
